e5b695ef14345f2843575037587af7465291671d
Matthias Haeussler <maeddes@gmail.com>
2018-09-03 14:44:55 +0200

    Added Exercises and Simple Todo List

---
+++ 01_exercise.md
@@ -0,0 +1,7 @@
+### Spring Boot Introduction
+
+- Build a simple Spring Boot Microservice with the following starter dependencies: Web, Actuator
+- Run the microservice and test the endpoint /actuator/health and /actuator/info
+- Annotate the Microservice with @RestController and implement a "Hello, World!" method, which responds to an HTTP GET request.
+- Add some basic "functionality" to make the service react to HTTP GET, POST, PUT and DELETE calls
+- Change the configuration to make the server run on port 8081 instead of 8080
--- intro/src/main/java/com/example/intro/IntroApplication.java
+++ intro/src/main/java/com/example/intro/IntroApplication.java
@@ -2,9 +2,36 @@ package com.example.intro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 @SpringBootApplication
+@RestController
 public class IntroApplication {
+
+	ArrayList<String> todos = new ArrayList<String>();
+
+	@GetMapping("/todos")
+	public ArrayList<String> getAll(){
+
+		return todos;
+	}
+
+	@PostMapping("/todos/{todo}")
+	public String add(@PathVariable String todo){
+
+		todos.add(todo);
+		return "added "+todo;
+	}
+
+	@DeleteMapping("/todos/{todo}")
+	public String remove(@PathVariable String todo){
+
+		todos.remove(todo);
+		return "remove "+todo;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(IntroApplication.class, args);
